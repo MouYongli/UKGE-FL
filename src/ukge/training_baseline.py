3dfd27aa00e3afa000181324670600269f133268
Yongli Mou <mouyongli@hotmail.com>
2024-06-07 18:54:40 +0200

    update training

--- src/ukge/training_baseline.py
+++ src/ukge/training_baseline.py
@@ -20,38 +20,6 @@ model_map = {
     'distmult': DistMult,
 }
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str.lower, default='distmult', choices=['distmult'])
-    parser.add_argument('--model_type', type=str.lower, default='logi', choices=['logi', 'rect'])
-    parser.add_argument('--loss_type', type=str.lower, default='logi', choices=['', 'rect'])
-    parser.add_argument('--dataset', type=str.lower, default='cn15k', choices=['cn15k', 'nl27k', 'ppi5k'])
-    parser.add_argument('--num_neg_per_positive', default=10, type=int)
-    parser.add_argument('--hidden_dim', default=128, type=int)
-    parser.add_argument('--num_epochs', default=100, type=int)
-    parser.add_argument('--batch_size', default=1024, type=int)
-    parser.add_argument('--lr', default=0.01, type=float)
-    args = parser.parse_args()
-
-    # Get the directory of the current script
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-
-    # Create a CSV file to save losses and metrics
-    train_log_file = os.path.join(script_dir, 'train_metrics.csv')
-    with open(train_log_file, 'w') as file:
-        file.write(','.join(['Epoch', 'Step', 'Loss', 'Loss pos', 'Loss neg']) + '\n')
-
-    val_log_file = os.path.join(script_dir, 'val_metrics.csv')
-    with open(val_log_file, 'w') as file:
-        file.write(','.join(['Epoch', 'Loss', 'Loss pos', 'Loss neg']) + '\n')
-    
-    train_dataset = KGTripleDataset(dataset=args.dataset, split='train', num_neg_per_positive=args.num_neg_per_positive)
-    val_dataset = KGTripleDataset(dataset=args.dataset, split='val')
-    test_dataset = KGTripleDataset(dataset=args.dataset, split='test')
-    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True)
-
 def get_t_ranks(model, h, r, ts, device):
     """
     Given some t index, return the ranks for each t
@@ -84,8 +52,37 @@ def get_t_ranks(model, h, r, ts, device):
 
     return ranks
 
-    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str.lower, default='distmult', choices=['distmult'])
+    parser.add_argument('--model_type', type=str.lower, default='logi', choices=['logi', 'rect'])
+    parser.add_argument('--loss_type', type=str.lower, default='logi', choices=['', 'rect'])
+    parser.add_argument('--dataset', type=str.lower, default='cn15k', choices=['cn15k', 'nl27k', 'ppi5k'])
+    parser.add_argument('--num_neg_per_positive', default=10, type=int)
+    parser.add_argument('--hidden_dim', default=128, type=int)
+    parser.add_argument('--num_epochs', default=100, type=int)
+    parser.add_argument('--batch_size', default=1024, type=int)
+    parser.add_argument('--lr', default=0.01, type=float)
+    args = parser.parse_args()
 
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Create a CSV file to save losses and metrics
+    train_log_file = os.path.join(script_dir, 'train_metrics.csv')
+    with open(train_log_file, 'w') as file:
+        file.write(','.join(['Epoch', 'Step', 'Loss', 'Loss pos', 'Loss neg']) + '\n')
+
+    val_log_file = os.path.join(script_dir, 'val_metrics.csv')
+    with open(val_log_file, 'w') as file:
+        file.write(','.join(['Epoch', 'Loss', 'Loss pos', 'Loss neg']) + '\n')
+    
+    train_dataset = KGTripleDataset(dataset=args.dataset, split='train', num_neg_per_positive=args.num_neg_per_positive)
+    val_dataset = KGTripleDataset(dataset=args.dataset, split='val')
+    test_dataset = KGTripleDataset(dataset=args.dataset, split='test')
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model_map[args.model](num_nodes=train_dataset.num_cons(), num_relations=train_dataset.num_rels(), hidden_channels=args.hidden_dim, model_type=args.model_type).to(device)
@@ -93,7 +90,6 @@ def get_t_ranks(model, h, r, ts, device):
     criterion_mse = nn.MSELoss()
     criterion_mae = nn.L1Loss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
 
     for epoch in range(args.num_epochs):
         model.train()
@@ -128,6 +124,7 @@ def get_t_ranks(model, h, r, ts, device):
         model.eval()
         val_loss_mse_total = 0.0
         val_loss_mae_total = 0.0
+        htrw_map = {}
         with torch.no_grad():
             for val_idx, (val_hrt, val_score) in enumerate(val_dataloader):
                 val_hrt, val_score = val_hrt.long(), val_score.float()
@@ -137,26 +134,27 @@ def get_t_ranks(model, h, r, ts, device):
                 val_loss_mse_total+= val_loss_mse.item()
                 val_loss_mae = criterion_mae(val_pred_score, val_score)
                 val_loss_mae_total+= val_loss_mae.item()
-                for i in range(len(val_hrt)):
-                h, r, t = val_hrt[i, 0].item(), val_hrt[i, 1].item(), val_hrt[i, 2].item()
-                tw_truth = [{'index': t, 'score': val_score[i].item()}]  # 此处应为实际的 ground truth 数据
-                ranks = get_t_ranks(model, h, r, [tw['index'] for tw in tw_truth], device)
-                gains = torch.tensor([tw['score'] for tw in tw_truth], device=device)
-                discounts = torch.log2(ranks + 1)
-                discounted_gains = gains / discounts
-                dcg = torch.sum(discounted_gains)  # discounted cumulative gain
-                max_possible_dcg = torch.sum(gains / torch.log2(torch.arange(len(gains), device=device) + 2))  # when ranks = [1, 2, ...len(truth)]
-                ndcg = dcg / max_possible_dcg  # normalized discounted cumulative gain
-                exp_gains = torch.tensor([2 ** tw['score'] - 1 for tw in tw_truth], device=device)
-                exp_discounted_gains = exp_gains / discounts
-                exp_dcg = torch.sum(exp_discounted_gains)
-                exp_max_possible_dcg = torch.sum(exp_gains / torch.log2(torch.arange(len(exp_gains), device=device) + 2))  # when ranks = [1, 2, ...len(truth)]
-                exp_ndcg = exp_dcg / exp_max_possible_dcg  # normalized discounted cumulative gain
-                ndcg_total += ndcg.item()
-                exp_ndcg_total += exp_ndcg.item()
 
-            avg_ndcg = ndcg_total / len(dataloader)
-            avg_exp_ndcg = exp_ndcg_total / len(dataloader)
+            #     for i in range(len(val_hrt)):
+            #         h, r, t = val_hrt[i, 0].item(), val_hrt[i, 1].item(), val_hrt[i, 2].item()
+            #         tw_truth = [{'index': t, 'score': val_score[i].item()}]  # 此处应为实际的 ground truth 数据
+            #         ranks = get_t_ranks(model, h, r, [tw['index'] for tw in tw_truth], device)
+            #         gains = torch.tensor([tw['score'] for tw in tw_truth], device=device)
+            #         discounts = torch.log2(ranks + 1)
+            #         discounted_gains = gains / discounts
+            #         dcg = torch.sum(discounted_gains)  # discounted cumulative gain
+            #         max_possible_dcg = torch.sum(gains / torch.log2(torch.arange(len(gains), device=device) + 2))  # when ranks = [1, 2, ...len(truth)]
+            #         ndcg = dcg / max_possible_dcg  # normalized discounted cumulative gain
+            #         exp_gains = torch.tensor([2 ** tw['score'] - 1 for tw in tw_truth], device=device)
+            #         exp_discounted_gains = exp_gains / discounts
+            #         exp_dcg = torch.sum(exp_discounted_gains)
+            #         exp_max_possible_dcg = torch.sum(exp_gains / torch.log2(torch.arange(len(exp_gains), device=device) + 2))  # when ranks = [1, 2, ...len(truth)]
+            #         exp_ndcg = exp_dcg / exp_max_possible_dcg  # normalized discounted cumulative gain
+            #         ndcg_total += ndcg.item()
+            #         exp_ndcg_total += exp_ndcg.item()
+
+            # avg_ndcg = ndcg_total / len(dataloader)
+            # avg_exp_ndcg = exp_ndcg_total / len(dataloader)
             with open(val_log_file, 'a') as file:
                 file.write(f"{epoch + 1},{val_loss_mse/len(val_dataloader):.4f},{val_loss_mae/len(val_dataloader):.4f}\n")
             print(f"Validation Loss MSE: {val_loss_mse/len(val_dataloader):.4f}, Loss MAE: {val_loss_mae/len(val_dataloader):.4f}")
